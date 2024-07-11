ab80dd6856250bad26aaa47ad0ffabc9bf9b1143
mitaka621 <d418201@gmail.com>
2024-07-11 22:57:30 +0300

    laptop dto added

---
+++ ChargingControllerApp/ChargingControllerApp/Models/LaptopDataModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChargingControllerApp.Models
+{
+	public class LaptopDataModel
+	{
+		private int batteryPercentage;
+
+		public DateTime CurrentDateTime { get; set; }
+
+		public bool IsCharging { get; set; }
+
+		public int BatteryPercentage
+		{
+			get => batteryPercentage;
+			set
+			{
+                if (value<0||value>100)
+                {
+					throw new ArgumentException("The battery percentage is invalid");
+				}
+
+				batteryPercentage = value;
+            }
+		}
+	}
+}
