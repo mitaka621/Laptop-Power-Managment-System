7fc566fd72a51e5ada7fefe8ae52a9c266f82a6a
ioni-art-tech <georgievaioana26@gmail.com>
2024-08-19 20:40:13 +0300

    bugs fixed

--- ChargingControllerApp/ChargingControllerApp/Properties/Resources.Designer.cs
+++ ChargingControllerApp/ChargingControllerApp/Properties/Resources.Designer.cs
@@ -103,6 +103,16 @@ namespace ChargingControllerApp.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap circle_xmark_regular2 {
+            get {
+                object obj = ResourceManager.GetObject("circle-xmark-regular2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap close {
             get {
                 object obj = ResourceManager.GetObject("close", resourceCulture);
